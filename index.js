0ba87683e3d99311db5ab173ac93937dba5bbc2e
Ben <allenben@protonmail.com>
2019-09-03 08:03:52 -0500

    Added .post

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // implement your API here
 const db = require('./data/db');
 const express = require('express');
+const bodyParser = require('body-parser');
 const server = express();
 const port = 8000;
+server.use(bodyParser());
 
 server.listen(port, () => console.log(`API running on port ${port}`))
+
+server.route('/api/users')
+    .post((req, res) => {
+        console.log(req.body);
+        let user = req.body;
+        if(!user.name || !user.bio){
+            res.status(400).json({ errorMessage: "Name or bio missing - please try again."});
+        } 
+    })
