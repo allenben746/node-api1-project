1eaaf05be5081b68715d61c76a3ed8c2f2f0765a
Ben <allenben@protonmail.com>
2019-09-03 08:11:58 -0500

    MVP complete

--- index.js
+++ index.js
@@ -43,4 +43,33 @@ server.route('/api/users')
             res.status(500).json({ error: "User unavailable." });
           })
       })
-     
+      .delete((req, res) => {
+        console.log(req.params.id);
+        let id = req.params.id;
+        db.remove(id)
+          .then(response => {
+            console.log(response);
+            if(response) res.status(200).json({ message: "User deleted." });
+            else res.status(404).json({ message: "User unavailable." });
+          })
+          .catch(e => {
+            console.log(e);
+            res.status(500).json({ error: "User unable to be removed." });
+          });
+      })
+      .put((req, res) => {
+        console.log(req.body);
+        let user = req.body;
+        if(!user.name || !user.bio) res.status(400).json({ errorMessage: "Name or bio missing - please try again."});
+        else db.update(req.params.id, user)
+          .then(response => {
+            console.log(response);
+            if(response) res.status(200).json({ message: "User updated."});
+            else res.status(404).json({ message: "User unavailable."});
+          })
+          .catch(e => {
+            console.log(e);
+            res.status(500).json({ error: "User unable to be edited." });
+          })
+      });
+    
