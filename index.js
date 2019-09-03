f63f616b53e69077e406ff094a6c0aa315467c45
Ben <allenben@protonmail.com>
2019-09-03 07:56:03 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // implement your API here
+const db = require('./data/db');
+const express = require('express');
+const server = express();
+const port = 8000;
+
+server.listen(port, () => console.log(`API running on port ${port}`))
